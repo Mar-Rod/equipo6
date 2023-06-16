664ee819251d4c6521774c13e76d5a265156977b
Andy-fer123 <116297504+Andy-fer123@users.noreply.github.com>
2023-06-16 11:17:43 -0600

    Add files via upload

--- actions.js
+++ actions.js
@@ -11,16 +11,3 @@ const newPassword = document.getElementsByClassName
       messageElement.textContent = "¡Contraseña recuperada con éxito!";
       messageElement.style.color = "#b40be7";
     });
-    const form= document.getElementsByClassName
-    console.log(form)
-    document.getElementById("form").addEventListener("submit", function(event) {
-      event.preventDefault();
-
-      var email = document.getElementById("email").value;
-      var newPassword = document.getElementById("form").value;
-
-      // Ejemplo para mostrar un mensaje de éxito en el mismo formulario
-      var messageElement = document.getElementById("message");
-      messageElement.textContent = "¡Registro con éxito!";
-      messageElement.style.color = "#b40be7";
-    });
